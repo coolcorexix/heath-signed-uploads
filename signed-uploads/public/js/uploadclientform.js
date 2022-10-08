ad7bf81614f86f504475514850f7a00d679d9577
phatIntern <phat.pham@saleshood.com>
2022-10-08 21:33:52 +0700

    feat: recording to start after the user presses play on the YouTube video embedded in an iframe.

--- signed-uploads/public/js/uploadclientform.js
+++ signed-uploads/public/js/uploadclientform.js
@@ -1,124 +1,115 @@
-document.addEventListener('DOMContentLoaded', async () => {
-	let camera_stream = null;
-	let media_recorder = null;
-	let blobs_recorded = [];
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
 
-	var startTime = Date.now();
-	var timeThreshold = 1000;
-	var detectPermissionDialog = function(allowed) {
-		console.log(allowed, Date.now() - startTime);
-		if (Date.now() - startTime > timeThreshold) {
-			
-		}
-		document.getElementById("tip-container").style.display = 'none';
-	};
-	var successCallback = function(stream) {
-		detectPermissionDialog(true);
-		
-		camera_stream = stream;
-		console.log(stream);
-		// set MIME type of recording as video/webm
-		media_recorder = new MediaRecorder(camera_stream, { mimeType: 'video/webm' });
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-		// event : new recorded video blob available 
-		media_recorder.addEventListener('dataavailable', function (e) {
-			// blobs_recorded.push(e.data);
-			sendPieceOfVideoToServer(e.data);
-			console.log('data available');
-		});
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    videoId: "dV-znS6RPbQ",
+    playerVars: { autoplay: 0, controls: 0 },
+    events: {
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
 
-		// start recording with each recorded blob having 1 second video
-		media_recorder.start(3000);
-		
-		document.getElementById("tip-container").setAttribute("data-prop", "set");
-		document.getElementById("tip-container").style.display = 'none';
-		document.getElementById("description").style.display = 'block';
-	};
-	var errorCallback = function(error) {
-		console.log(error);
-		if ((error.name == 'NotAllowedError') ||
-			(error.name == 'PermissionDismissedError')) {
-			detectPermissionDialog(false);
-		}
+async function onPlayerStateChange(e) {
+  let camera_stream = null;
+  let media_recorder = null;
 
-		document.getElementById("tip-container").setAttribute("data-prop", "set");
-		document.getElementById("tip-container").style.display = 'none';
-		document.getElementById("description").style.display = 'block';
-		alert("Please allow camera and microphone and reload the page to continue.");
-	};
+  var startTime = Date.now();
+  var timeThreshold = 1000;
+  var detectPermissionDialog = function (allowed) {
+    console.log(allowed, Date.now() - startTime);
+    if (Date.now() - startTime > timeThreshold) {
+    }
+    document.getElementById("tip-container").style.display = "none";
+  };
+  var successCallback = function (stream) {
+    detectPermissionDialog(true);
 
-	setTimeout(function() {
-		if(document.getElementById("tip-container").getAttribute("data-prop") !== "set")
-			document.getElementById("tip-container").style.display = 'block';
-	}, 1000);
-	navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(successCallback, errorCallback);
+    camera_stream = stream;
+    console.log(stream);
+    // set MIME type of recording as video/webm
+    media_recorder = new MediaRecorder(camera_stream, {
+      mimeType: "video/webm",
+    });
 
-	// event : recording stopped & all blobs sent
-	//media_recorder.addEventListener('stop', function () {
-		// create local object URL from the recorded video blobs
-		//let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
-		//download_link.href = video_local;
-	//});
+    // event : new recorded video blob available
+    media_recorder.addEventListener("dataavailable", function (e) {
+      sendPieceOfVideoToServer(e.data);
+    });
 
-	//	Cloudinary
-	const signResponse = await fetch('/api/signuploadform');
-	const signData = await signResponse.json();
+    // start recording with each recorded blob having 3 seconds video
+    media_recorder.start(3000);
 
-	function sendPieceOfVideoToServer(eventData) {
-		const formData = new FormData();
-	
-		// Append parameters to the form data. The parameters that are signed using 
-		// the signing function (signuploadform) need to match these.
-		formData.append("file", new Blob([eventData], { type: 'video/webm' }));
-		formData.append("api_key", signData.apikey);
-		formData.append("timestamp", signData.timestamp);
-		formData.append("signature", signData.signature);
-		formData.append("transformation", "q_50");
-		formData.append("eager", "c_fill,g_auto:face,h_300,w_300");
-		formData.append("eager_async", "true");
-		formData.append("folder", "reaction_videos");
+    document.getElementById("tip-container").setAttribute("data-prop", "set");
+    document.getElementById("tip-container").style.display = "none";
+    document.getElementById("description").style.display = "block";
+  };
+  var errorCallback = function (error) {
+    console.log(error);
+    if (
+      error.name == "NotAllowedError" ||
+      error.name == "PermissionDismissedError"
+    ) {
+      detectPermissionDialog(false);
+    }
 
-		fetch(url, {
-			method: "POST",
-			body: formData
-		})
-	}
+    document.getElementById("tip-container").setAttribute("data-prop", "set");
+    document.getElementById("tip-container").style.display = "none";
+    document.getElementById("description").style.display = "block";
+    alert(
+      "Please allow camera and microphone and reload the page to continue."
+    );
+  };
 
-	const url = "https://api.cloudinary.com/v1_1/" + signData.cloudname + "/auto/upload";
-	const form = document.querySelector("form");
+  setTimeout(function () {
+    if (
+      document.getElementById("tip-container").getAttribute("data-prop") !==
+      "set"
+    )
+      document.getElementById("tip-container").style.display = "block";
+  }, 1000);
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then(successCallback, errorCallback);
 
-	form.addEventListener("submit", (e) => {
-		e.preventDefault();
+  // event : recording stopped & all blobs sent
+  //media_recorder.addEventListener('stop', function () {
+  // create local object URL from the recorded video blobs
+  //let video_local = URL.createObjectURL(new Blob(blobs_recorded, { type: 'video/webm' }));
+  //download_link.href = video_local;
+  //});
 
-		const formData = new FormData();
+  //	Cloudinary
+  const signResponse = await fetch("/api/signuploadform");
+  const signData = await signResponse.json();
+  const url =
+    "https://api.cloudinary.com/v1_1/" + signData.cloudname + "/auto/upload";
 
-		// Append parameters to the form data. The parameters that are signed using 
-		// the signing function (signuploadform) need to match these.
-		media_recorder.stop();
-		formData.append("file", new Blob(blobs_recorded, { type: 'video/webm' }));
-		formData.append("api_key", signData.apikey);
-		formData.append("timestamp", signData.timestamp);
-		formData.append("signature", signData.signature);
-		formData.append("transformation", "q_50");
-		formData.append("eager", "c_fill,g_auto:face,h_300,w_300");
-		formData.append("eager_async", "true");
-		formData.append("folder", "reaction_videos");
+  function sendPieceOfVideoToServer(eventData) {
+    const formData = new FormData();
 
-		fetch(url, {
-			method: "POST",
-			body: formData
-		})
-		
-		        .then((response) => {
-                                return response.text();
-                        })
-                        .then((data) => {
-                                alert(data);
-                                //console.log(JSON.parse(data))
-                                //var str = JSON.stringify(JSON.parse(data), null, 4);
-                                //document.getElementById("formdata").innerHTML += str;
-                        });	
-	});
+    // Append parameters to the form data. The parameters that are signed using
+    // the signing function (signuploadform) need to match these.
+    formData.append("file", new Blob([eventData], { type: "video/webm" }));
+    formData.append("api_key", signData.apikey);
+    formData.append("timestamp", signData.timestamp);
+    formData.append("signature", signData.signature);
+    formData.append("transformation", "q_50");
+    formData.append("eager", "c_fill,g_auto:face,h_300,w_300");
+    formData.append("eager_async", "true");
+    formData.append("folder", "reaction_videos");
 
-})
-
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+  }
+}
